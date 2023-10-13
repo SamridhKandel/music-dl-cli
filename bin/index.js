d20ebc400ed95aa21113e7a01ae2ba81c9d3da4a
Samridh Kandel <samridh.8848@gmail.com>
2023-10-13 23:04:33 +0545

    "added metadata to the mp3"

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,33 @@
 #! /usr/bin/env node 
 import  yargs from 'yargs'
 import  ytdl from 'ytdl-core';
+import https from 'https';
 import fs from 'fs';
 import ffmpeg from 'fluent-ffmpeg';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import 'dotenv/config';
-import fetch from 'node-fetch';
-import spotify from 'spotify-url-info';
+import SpotifyWebApi from 'spotify-web-api-node';
 import SpotifyToYouTubeMusic from 'spotify-to-ytmusic';
+import NodeID3 from 'node-id3';
 
 
 
+const _clientSecret = process.env.CLIENT_SECRET;
+const _clientID = process.env.CLIENT_ID;
 
-const clientSecret = process.env.CLIENT_SECRET;
-const clientID = process.env.CLIENT_ID;
-console.log(clientSecret);
+const spotifyWebApi = new SpotifyWebApi({
+    clientId: `${_clientID}`,
+    clientSecret: `${_clientSecret}`,
+    redirectUri: "https://example.com/callback",
+})
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const spotifyToYouTubeMusic = await SpotifyToYouTubeMusic ({
-    clientID: `${clientID}`,
-    clientSecret: `${clientSecret}`,
+    clientID: `${_clientID}`,
+    clientSecret: `${_clientSecret}`,
 });
 
 
@@ -31,37 +36,62 @@ const spotifyToYouTubeMusic = await SpotifyToYouTubeMusic ({
 class SongData {
     constructor(){} 
 
-    async getTrackData() {
+    async getTrack() {
         const songNameArray = yargs(process.argv.splice(2)).argv._; 
-        var data = spotify(fetch);
-        var r =  await data.getPreview(songNameArray[0]);
-        this.trackName = r.title;
-        this.trackArtist = r.artist;
-        this.trackID = await spotifyToYouTubeMusic(songNameArray[0]);
-        console.log(r);
+        await spotifyWebApi.clientCredentialsGrant().then((data)=> {
+            spotifyWebApi.setAccessToken(data.body['access_token']);
+        })
+        var spotifyData = await spotifyWebApi.getTrack(`${songNameArray[0].slice(31,53)}`);
+        var r = spotifyData.body;
+        this.trackArtist = (r.artists[0].name);
+        this.trackName = r.name;
+        this.trackDate = r.album.release_date;
+        this.trackNumber = r.track_number;
+        this.trackAlbum = r.album.name;
+        this.trackID = await spotifyToYouTubeMusic(r.id); 
+        this.imagePath = `${__dirname}/temp-${this.trackName}-${this.trackArtist}.jpg`
+        var imageFile = fs.createWriteStream(this.imagePath);
+        const req = https.get(r.album.images[0].url, response => {
+            response.pipe(imageFile);
+        });
+
+        imageFile.on('finish', ()=>{
+            console.log("Album Cover Downloaded");
+        })
+
+        let stream = ytdl(`${track.trackID}`, {quality:'highestaudio'})
+        let start = Date.now();
+
+        ffmpeg(stream)
+        .audioBitrate(128)
+        .save(`${__dirname}/${track.trackArtist}- ${track.trackName}.mp3`)
+        .on('progess',p => {
+            readline.cursorTo(process.stdout, 0);
+            process.stdout.write(`${p.targetSize}kb downloaded`);
+        })
+        .on('end', ()=>{
+            var metadata = {
+                title: `${this.trackName}`,
+                artist: `${this.trackArtist}`,
+                album: `${this.trackAlbum}`,
+                APIC: `${this.imagePath}`,
+                TRCK: `${this.trackNumber}`
+            }
+             var success  = NodeID3.write(metadata, `${__dirname}/${this.trackArtist}- ${this.trackName}.mp3`)
+             
+             if(success) {
+                console.log("Successfully wrote metadata");
+                fs.unlink(`${this.imagePath}`, err => {
+                    return;
+                });
+             } 
+            console.log(`\nSuccessfully Downloaded ${this.trackName} - ${this.trackArtist} - ${(Date.now() - start) / 1000}s`);
+        }); 
     }
-        
 }
 
 
+
+
 const track = new SongData();
-await track.getTrackData();
-console.log(track);
-
-
-let stream = ytdl(`${track.trackID.slice(28)}`, {quality:'highestaudio'})
-console.log(stream);
-let start = Date.now();
-
-ffmpeg(stream)
-.audioBitrate(128)
-.save(`${__dirname}/${track.trackArtist}- ${track.trackName}}.mp3`)
-.on('progess',p => {
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(`${p.targetSize}kb downloaded`);
-})
-.on('end', ()=>{
-    console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-});
-
-
+await track.getTrack();
