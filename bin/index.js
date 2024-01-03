2187d038253170dc8036d98e387808ef93ea7b21
Samridh Kandel <samridh.8848@gmail.com>
2024-01-03 20:47:31 +0545

    "Added playlist downloads + error handling"

--- bin/index.js
+++ bin/index.js
@@ -1,97 +1,163 @@
-#! /usr/bin/env node 
-import  yargs from 'yargs'
-import  ytdl from 'ytdl-core';
-import https from 'https';
-import fs from 'fs';
-import ffmpeg from 'fluent-ffmpeg';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import 'dotenv/config';
-import SpotifyWebApi from 'spotify-web-api-node';
-import SpotifyToYouTubeMusic from 'spotify-to-ytmusic';
-import NodeID3 from 'node-id3';
-
-
+#! /usr/bin/env node
+import yargs from "yargs";
+import ytdl from "ytdl-core";
+import https from "https";
+import fs from "fs";
+import ffmpeg from "fluent-ffmpeg";
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+import "dotenv/config";
+import SpotifyWebApi from "spotify-web-api-node";
+import SpotifyToYouTubeMusic from "spotify-to-ytmusic";
+import NodeID3 from "node-id3";
+import { homedir } from "os";
+import progress from "progress";
+import ProgressBar from "progress";
 
 const _clientSecret = process.env.CLIENT_SECRET;
 const _clientID = process.env.CLIENT_ID;
 
 const spotifyWebApi = new SpotifyWebApi({
-    clientId: `${_clientID}`,
-    clientSecret: `${_clientSecret}`,
-    redirectUri: "https://example.com/callback",
-})
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const spotifyToYouTubeMusic = await SpotifyToYouTubeMusic ({
-    clientID: `${_clientID}`,
-    clientSecret: `${_clientSecret}`,
+  clientId: `${_clientID}`,
+  clientSecret: `${_clientSecret}`,
+  redirectUri: "https://example.com/callback",
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(homedir(), "Music");
 
+const spotifyToYouTubeMusic = await SpotifyToYouTubeMusic({
+  clientID: `${_clientID}`,
+  clientSecret: `${_clientSecret}`,
+});
 
+await spotifyWebApi.clientCredentialsGrant().then((data) => {
+  spotifyWebApi.setAccessToken(data.body["access_token"]);
+});
 
-class SongData {
-    constructor(){} 
+class Playlist {
+  constructor() {}
 
-    async getTrack() {
-        const songNameArray = yargs(process.argv.splice(2)).argv._; 
-        await spotifyWebApi.clientCredentialsGrant().then((data)=> {
-            spotifyWebApi.setAccessToken(data.body['access_token']);
-        })
-        var spotifyData = await spotifyWebApi.getTrack(`${songNameArray[0].slice(31,53)}`);
+  async getPlaylistInfo() {
+    var playlistSongsArray = new Array();
+    const playlist = yargs(process.argv.splice(2)).argv._;
+    var playlistID = playlist[0].slice(34, 56);
+    console.log("playlistId: " + playlistID);
+    var spotifyData = await spotifyWebApi.getPlaylist(playlistID);
+    var r = spotifyData.body;
+    var i = 0;
+    r.tracks.items.forEach(() => {
+      playlistSongsArray.push(r.tracks.items[i].track.id);
+      i++;
+    });
+    return playlistSongsArray;
+  }
+}
+
+class Song {
+  constructor() {}
+
+  async getTrack(spotifyID) {
+    try {
+      return new Promise(async (resolve, reject) => {
+        var spotifyData = await spotifyWebApi.getTrack(spotifyID);
         var r = spotifyData.body;
-        this.trackArtist = (r.artists[0].name);
+        this.trackArtist = r.artists[0].name;
         this.trackName = r.name;
         this.trackDate = r.album.release_date;
         this.trackNumber = r.track_number;
         this.trackAlbum = r.album.name;
-        this.trackID = await spotifyToYouTubeMusic(r.id); 
-        this.imagePath = `${__dirname}/temp-${this.trackName}-${this.trackArtist}.jpg`
+        this.trackID = await spotifyToYouTubeMusic(r.id);
+        this.imagePath = `${__dirname}/temp-${this.trackName}-${this.trackArtist}.jpg`;
         var imageFile = fs.createWriteStream(this.imagePath);
-        const req = https.get(r.album.images[0].url, response => {
-            response.pipe(imageFile);
+        const req = https.get(r.album.images[0].url, (response) => {
+          response.pipe(imageFile);
         });
 
-        imageFile.on('finish', ()=>{
-            console.log("Album Cover Downloaded");
-        })
+        imageFile.on("finish", () => {
+          console.log("Album Cover Downloaded");
+        });
+        let stream = ytdl(`${this.trackID}`, { quality: "highestaudio" }).on(
+          "error",
+          (err) => {
+            console.log("Error Downloading Track" + err);
+            resolve();
+          }
+        );
 
-        let stream = ytdl(`${track.trackID}`, {quality:'highestaudio'})
         let start = Date.now();
 
-        ffmpeg(stream)
-        .audioBitrate(128)
-        .save(`${__dirname}/${track.trackArtist}- ${track.trackName}.mp3`)
-        .on('progess',p => {
-            readline.cursorTo(process.stdout, 0);
-            process.stdout.write(`${p.targetSize}kb downloaded`);
-        })
-        .on('end', ()=>{
-            var metadata = {
+        try {
+          ffmpeg(stream)
+            .audioBitrate(128)
+            .save(`${__dirname}/${this.trackArtist}- ${this.trackName}.mp3`)
+            .on("progress", (p) => {
+              let len = parseInt(p.targetSize, 10);
+              let bar = new progress(
+                "downloading [:bar] :rate/bps :percent :etas",
+                {
+                  complete: "=",
+                  incomplete: " ",
+                  width: 20,
+                  total: len,
+                }
+              );
+              process.stdout.write(`${p.targetSize}kb downloaded`);
+              bar.tick(p.length);
+            })
+
+            .on("end", () => {
+              var metadata = {
                 title: `${this.trackName}`,
                 artist: `${this.trackArtist}`,
                 album: `${this.trackAlbum}`,
                 APIC: `${this.imagePath}`,
-                TRCK: `${this.trackNumber}`
-            }
-             var success  = NodeID3.write(metadata, `${__dirname}/${this.trackArtist}- ${this.trackName}.mp3`)
-             
-             if(success) {
+                TRCK: `${this.trackNumber}`,
+              };
+              var success = NodeID3.write(
+                metadata,
+                `${__dirname}/${this.trackArtist}- ${this.trackName}.mp3`
+              );
+
+              if (success) {
                 console.log("Successfully wrote metadata");
-                fs.unlink(`${this.imagePath}`, err => {
-                    return;
+                fs.unlink(`${this.imagePath}`, (err) => {
+                  return;
                 });
-             } 
-            console.log(`\nSuccessfully Downloaded ${this.trackName} - ${this.trackArtist} - ${(Date.now() - start) / 1000}s`);
-        }); 
+              }
+              resolve();
+              console.log(
+                `\nSuccessfully Downloaded ${this.trackName} - ${
+                  this.trackArtist
+                } - ${(Date.now() - start) / 1000}s`
+              );
+            })
+            .on("error", () => {
+              console.log("Error!!!");
+              fs.unlink(`${this.imagePath}`, (err) => {
+                return;
+              });
+              resolve();
+            });
+        } catch (err) {
+          console.error("Error downloading track:", err);
+          resolve();
+        }
+      });
+    } catch (error) {
+      log("error downloading track");
     }
+  }
 }
 
-
-
-
-const track = new SongData();
-await track.getTrack();
+const playlist = new Playlist();
+var z = new Song();
+var arr = await playlist.getPlaylistInfo();
+for (const songs of arr) {
+  try {
+    await z.getTrack(songs);
+  } catch (error) {
+    console.error("error");
+    resolve();
+  }
+}
